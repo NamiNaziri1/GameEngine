ecf9c82f6e4e0bf6196d4aa3558cb581aa2b9e07
NamiNaziri <naminaziri1@gmail.com>
2019-09-17 23:00:10 +0430

    Game loop and keyboard event

--- GameEngin1.0/GameEngin1.0/Main.cpp
+++ GameEngin1.0/GameEngin1.0/Main.cpp
@@ -1,9 +1,44 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "It's UP");
+	window.setFramerateLimit(60);
 
 
+	window.setKeyRepeatEnabled(false);
+
+	bool play = true;
+	sf::Event event;
+	//Game Loop
+	while (play)
+	{
+		//EVENTS
+
+		while (window.pollEvent(event)) 
+		{
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A) 
+			{
+				std::cout << "key has been pressed" << std::endl;
+			}
+
+			if (event.type == sf::Event::Closed) 
+			{
+				play = false;
+			}
+		}
+
+		//LOGIC
+
+		//RENDERING
+		window.clear();
+
+
+		window.display();
+	}
+
+	//clean
+
+	window.close();
 	return 0;
 }
